e3a14e539f3536ad9eb012c93041590ffbe0874a
maxit <maxit@DESKTOP-ONJ4NHL>
2024-12-23 00:06:48 +0500

    Добавьте файлы проекта.

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using CalcHome;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Windows;
+
+namespace calcmainmenutest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange
+            Window window = new Window();
+            // Act
+            window.Show(); // открываем окно
+            window.Close(); // закрываем окно
+            // Assert
+            Assert.IsFalse(window.IsVisible); // проверяем, что окно больше не отображается
+
+
+
+        }
+    }
+}
